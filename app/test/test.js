20716defc5609d52d2bc5180cdf426a18718767a
xero <zerosoftwere@gmail.com>
2017-07-01 13:06:19 +0100

    Small changes to tests suite

--- app/test/test.js
+++ app/test/test.js
@@ -1,9 +1,9 @@
-const expect = require('chai').expect;
+const assert = require('assert');
 
 describe('Array', function() {
     describe('indexOf()', function() {
         it('Should return -1 when the value is not present',function() {
-            expect([1, 2, 3].indexOf(4)).to.equal(-1);
+            assert.equal([1, 2, 3].indexOf(4), -1);
         });
     });
 })
