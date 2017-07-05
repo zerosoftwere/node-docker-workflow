629f14feee2452002b16a673bfa1d5824e0262aa
xero <zerosoftwere@gmail.com>
2017-07-05 16:25:16 +0100

    Enhanced ui view with ejs

--- app/server.js
+++ app/server.js
@@ -3,15 +3,18 @@ const redis = require('redis');
 const logger = require('morgan');
 const path = require('path');
 
-var client = redis.createClient({port: 6379, host: 'redis'});
+var client = redis.createClient({port: 6379, host: 'localhost'});
 
 var app = express();
+app.use(express.static(path.join(__dirname, 'static')));
 app.use(logger('combined'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res, next) {
     client.incr('visits', function(err, visits) {
         if (err) return next(err);
-        res.status(200).send('This page has been viewed ' + visits + ' times');
+        res.render('pages/index', {visits: visits});
     });
 });
 
